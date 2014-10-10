b7070fa801f54f3d6f08e9060ea25c743d4a5225
Felipe da Veiga Leprevost <leprevost@cpan.org>
2014-10-10 14:33:34 +0000

    Import of LEPREVOST/EveOnline-Api-0.04 from CPAN.
    gitpan-cpan-distribution: EveOnline-Api
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         LEPREVOST/EveOnline-Api-0.04.tar.gz
    gitpan-cpan-author:       LEPREVOST
    gitpan-cpan-maturity:     released

--- lib/EveOnline/Api.pm
+++ lib/EveOnline/Api.pm
@@ -21,11 +21,11 @@ EveOnline::Api - the Perl version of the Eve Online API system.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 has 'apiroot'   =>  (
     is      =>  'rw',
